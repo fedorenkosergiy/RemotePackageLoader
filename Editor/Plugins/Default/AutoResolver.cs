869fb2c7e08a85a140262c95d5689abdba805487
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2022-05-19 00:33:05 +0300

    * auto resolver

--- Editor/Plugins/Default/AutoResolver.cs
+++ Editor/Plugins/Default/AutoResolver.cs
@@ -1,8 +1,4 @@
-using System.Diagnostics;
-using System.IO;
 using UnityEditor;
-using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 namespace RemotePackageLoader.Editor
 {
@@ -11,31 +7,8 @@ namespace RemotePackageLoader.Editor
     {
         static AutoResolver()
         {
-            return;
-            Resolve();
-        }
-
-        [MenuItem("rpl/resolve")]
-        private static void Resolve()
-        {
             Resolver resolver = new Resolver();
             resolver.Resolve();
-
-            return;
-            string name = "moqunity-0.2.1";
-            string project = Application.dataPath.Remove(Application.dataPath.Length - 6, 6);
-            string tgz = project + "Packages/" + name + ".tgz";
-            
-            string tempTar = FileUtil.GetUniqueTempPathInProject();
-            
-            ArchiveExtractorCreator extractorCreator = new ArchiveExtractorCreator();
-            ArchiveExtractor extractor = extractorCreator.Create();
-            extractor.Extract(tgz, tempTar);
-
-            string tar = tempTar + "/" + name + ".tar";
-            string content = project + name;
-            
-            extractor.Extract(tar, content);
         }
     }
 }
