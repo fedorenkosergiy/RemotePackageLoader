6253c87db29d0f0f0cf3999653b9163b2da20ce3
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2022-05-21 00:35:07 +0300

    * minor

--- Editor/Plugins/Default/Resolver.cs
+++ Editor/Plugins/Default/Resolver.cs
@@ -4,14 +4,14 @@ namespace RemotePackageLoader.Editor
     {
         public void Resolve()
         {
-            ManifestReader reader = new();
+            ManifestReader reader = new ManifestReader();
             Manifest manifest = reader.Read();
             for (int i = 0; i < manifest.Packages.Count; ++i)
             {
                 RemotePackageInfo info = manifest.Packages[i];
-                PackagesUpdater updater = new();
+                PackagesUpdater updater = new PackagesUpdater();
                 if (updater.Exists(info)) continue;
-                RemotePackageDownloader downloader = new();
+                RemotePackageDownloader downloader = new RemotePackageDownloader();
                 string archivePath = downloader.Download(info);
                 Unpacker unpacker = new Unpacker();
                 unpacker.Unpack(info, archivePath);
